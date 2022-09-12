30f5c606241d81597ca60e5227eec013212b8ebb
DImas-12 <dimasadit363@gmail.com>
2022-09-12 22:08:45 +0700

    creat post

--- src/app/home/card/card.component.ts
+++ src/app/home/card/card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-card',
@@ -6,10 +7,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.scss'],
 })
 export class CardComponent implements OnInit {
+  @Input() data!: any;
+  FormComment!: FormGroup;
+
+  Comment!: boolean;
+  T_Comment!: boolean;
+  T_Close!: boolean;
+
+  dataComment: any = [];
   constructor() {}
 
-  ngOnInit(): void {}
-  longText = `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog
-  from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was
-  originally bred for hunting.`;
+  ngOnInit(): void {
+    console.log('data nya haha', this.data);
+    this.FormGroup();
+    this.Comment = false;
+    this.T_Comment = true;
+    this.T_Close = false;
+  }
+
+  FormGroup() {
+    this.FormComment = new FormGroup({
+      comment: new FormControl(null, [Validators.required]),
+    });
+  }
+  Submit() {
+    if (this.FormComment.status == 'INVALID') {
+      alert('please complete the Comment form');
+    } else if (this.FormComment.status == 'VALID') {
+      this.dataComment.push(this.FormComment.value);
+    }
+  }
+  openComment() {
+    this.Comment = true;
+    this.T_Comment = false;
+    this.T_Close = true;
+  }
+  closeComment() {
+    this.Comment = false;
+    this.T_Comment = true;
+    this.T_Close = false;
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  dataPost: any = [];
+  FormPost!: FormGroup;
+  constructor() {}
 
   ngOnInit(): void {
+    this.FormGroup();
   }
 
+  FormGroup() {
+    this.FormPost = new FormGroup({
+      title: new FormControl(null, [Validators.required]),
+      content: new FormControl(null, Validators.required),
+    });
+  }
+  Submit() {
+    console.log('data submit', this.FormPost.status);
+    if (this.FormPost.status == 'INVALID') {
+      alert('please complete the form');
+    } else if (this.FormPost.status == 'VALID') {
+      this.dataPost.push(this.FormPost.value);
+    }
+  }
+  test() {
+    const tmp = {
+      produk: 'haha',
+    };
+    this.dataPost.push(tmp);
+    console.log('haha', tmp);
+  }
 }
